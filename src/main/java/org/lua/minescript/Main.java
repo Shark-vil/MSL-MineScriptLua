09d4df6acdb082f9046cbda760ea53e1a6d10986
Shark-vil <shark_vil@mail.ru>
2020-11-09 17:00:59 +0300

    Added plugin initializer

--- src/main/java/org/lua/minescript/Main.java
+++ src/main/java/org/lua/minescript/Main.java
@@ -1,5 +1,20 @@
 package org.lua.minescript;
 
-public class Main {
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.ConsoleCommandSender;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin {
+    public static final String pluginName = "MineScript";
+    public static final ConsoleCommandSender console = Bukkit.getServer().getConsoleSender();
+    public static Plugin plugin;
+
+    @Override
+    public void onEnable() {
+        console.sendMessage(ChatColor.AQUA + "Loading the \""+ pluginName + "\" plugin");
+        plugin = this;
+        console.sendMessage(ChatColor.AQUA + "Plugin \""+ pluginName + "\" loaded");
+    }
 }
